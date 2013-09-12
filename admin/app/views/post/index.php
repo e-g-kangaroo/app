8c2df522b03c53e02866770882a575864c7963a4
niaeashes <nia.eashes@gmail.com>
2013-09-13 03:51:38 +0900

    linked to the edit apge from post index

--- admin/app/views/post/index.php
+++ admin/app/views/post/index.php
@@ -1,6 +1,6 @@
-<?php foreach ($posts as $post): ?>
-<div class="container">
-	<h3><?= $post->title() ?></h3>
-	<p><?= $post->content() ?></p>
-</div>
-<?php endforeach; ?>
+	<?php foreach ($posts as $post): ?>
+	<div class="container">
+		<h3><?= Html::anchor('post/'.$type.'/edit/'.$post->id, $post->title()) ?></h3>
+		<p><?= $post->content() ?></p>
+	</div>
+	<?php endforeach; ?>
