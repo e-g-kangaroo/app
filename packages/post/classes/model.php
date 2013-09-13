b7a18a4335b526e9aeef45ab76247537a96e7379
niaeashes <nia.eashes@gmail.com>
2013-09-13 14:07:38 +0900

    added find_published() on \Post\Model

--- packages/post/classes/model.php
+++ packages/post/classes/model.php
@@ -57,4 +57,12 @@ abstract class Model extends \Orm\Model
 
 		return parent::find($id, $options);
 	}
+
+
+	public static function find_piblished($id = null, array $options = array())
+	{
+		$options = \Arr::merge($options, static::$_publish_conditions);
+
+		return parent::find($id, $options);
+	}
 }
