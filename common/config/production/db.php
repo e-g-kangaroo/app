4cecbd95a57ac79aa89db5c652c838d779d92ab2
niaeashes <nia.eashes@gmail.com>
2013-09-17 09:52:29 +0900

    default db config in production env

---
+++ common/config/production/db.php
@@ -0,0 +1,12 @@
+<?php
+
+return array(
+	'default' => array(
+		'connection'  => array(
+			'dsn'        => 'mysql:host=127.0.0.1;dbname=skyroof',
+			'username'   => 'root',
+			'password'   => '',
+		),
+		'profiling' => true,
+	),
+);
