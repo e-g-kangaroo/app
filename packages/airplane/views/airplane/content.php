777ce6fffc7ce51963e2930b56505d9154882d8c
niaeashes <nia.eashes@gmail.com>
2013-09-17 11:16:18 +0900

    remove airplane package

--- packages/airplane/views/airplane/content.php
+++
@@ -1,4 +0,0 @@
-	<div class="form-group">
-		<label><?= Inflector::humanize($name) ?></label>
-		<textarea class="form-control form-control-content" rows="12" name="<?= $name ?>"><?= Input::post($name, $object->{$name}) ?></textarea>
-	</div>
--- packages/airplane/views/airplane/datetime.php
+++
@@ -1,4 +0,0 @@
-	<div class="form-group">
-		<label><?= Inflector::humanize($name) ?></label>
-		<input type="datetime" class="form-control" name="<?= $name ?>" value="<?= Input::post($name, $object->{$name}) ?>">
-	</div>
