0cc25ff52d98a435b88d0c9be667c22fcbc447b4
niaeashes <nia.eashes@gmail.com>
2013-09-17 11:08:24 +0900

    Remove skyroof package

--- packages/skyroof/bootstrap.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-Autoloader::add_classes(array(
-	'Skyroof' => __DIR__.'/classes/skyroof.php',
-	'Skyroof\\Command\\Generate' => __DIR__.'/classes/command/generate.php',
-	'Skyroof\\Command\\Generate_Post' => __DIR__.'/classes/command/generate/post.php',
-	'Skyroof\\Option' => __DIR__.'/classes/option.php',
-));
