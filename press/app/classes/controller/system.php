1c99dc4a40a9b461b5a25e5c861e610952efc47d
niaeashes <nia.eashes@gmail.com>
2013-09-22 13:26:40 +0900

    Added robots.txt

--- press/app/classes/controller/system.php
+++ press/app/classes/controller/system.php
@@ -6,4 +6,14 @@ class Controller_System extends Controller_Template
 	{
 		$this->template = View::forge('404');
 	}
+
+	public function action_robots()
+	{
+		$this->template = "User-agent: *\nAllow: /";
+
+		$response = new Response("User-agent: *\nAllow: /", 200);
+		$response->set_header('Content-Type', 'text/plane; charset=utf-8');
+
+		return $response;
+	}
 }
--- press/app/config/routes.php
+++ press/app/config/routes.php
@@ -2,4 +2,5 @@
 
 return array(
 	'_404_' => 'system/404',
+	'robots' => 'system/robots',
 );
